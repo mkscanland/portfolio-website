8d74167614349afa2414f2e64f4ed29121e226b0
mkscanland <100780459+mkscanland@users.noreply.github.com>
2026-09-18 23:03:26 -0400

    Measure mobile title text bounds in browser test

--- tests/mobile-layout.spec.js
+++ tests/mobile-layout.spec.js
@@ -11,16 +11,29 @@ for (const width of [320, 360]) {
 
     await expect(title).toHaveCSS('white-space', 'nowrap')
 
-    const layout = await title.evaluate((element) => ({
-      clientHeight: element.clientHeight,
-      clientWidth: element.clientWidth,
-      scrollHeight: element.scrollHeight,
-      scrollWidth: element.scrollWidth,
-    }))
-    const documentWidth = await page.evaluate(() => document.documentElement.scrollWidth)
+    const layout = await title.evaluate((element) => {
+      const titleRect = element.getBoundingClientRect()
+      const textRange = document.createRange()
+      textRange.selectNodeContents(element)
 
-    expect(layout.scrollHeight).toBeLessThanOrEqual(layout.clientHeight)
-    expect(layout.scrollWidth).toBeLessThanOrEqual(layout.clientWidth)
-    expect(documentWidth).toBeLessThanOrEqual(width)
+      const textRects = [...textRange.getClientRects()]
+
+      return {
+        documentWidth: document.documentElement.scrollWidth,
+        titleRect: {
+          left: titleRect.left,
+          right: titleRect.right,
+        },
+        textRects: textRects.map((rect) => ({
+          left: rect.left,
+          right: rect.right,
+        })),
+      }
+    })
+
+    expect(layout.textRects).toHaveLength(1)
+    expect(layout.textRects[0].left).toBeGreaterThanOrEqual(layout.titleRect.left)
+    expect(layout.textRects[0].right).toBeLessThanOrEqual(layout.titleRect.right)
+    expect(layout.documentWidth).toBeLessThanOrEqual(width)
   })
 }
